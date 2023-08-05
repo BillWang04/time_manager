d4b805b5b59fbba83c95c34856d1ea6240df8bd0
Bill Wang <18billwang@gmail.com>
2023-08-05 15:11:01 -0700

    Update Create Button and shows Container

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Tab Manager: Solving the Problem of Too Many Tabs
+Function:
+    Everytime a new tab is open on google chrome, a small window will pop up 
+    and ask for where to  
---
+++ popup.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const createButton = document.getElementById("createButton");
+    const addButton = document.getElementById("addButton");
+    const createGroupContainer = document.getElementById("createGroupContainer");
+    const existingGroupsContainer = document.getElementById("existingGroups");
+    const newGroupNameInput = document.getElementById("newGroupName");
+  
+    // Function to show/hide elements
+    function toggleElement(element, show) {
+      if (show) {
+        element.classList.remove("hidden");
+      } else {
+        element.classList.add("hidden");
+      }
+    }
+  
+    // Show/hide create group input on button click
+    createButton.addEventListener("click", function () {
+      toggleElement(createGroupContainer, true);
+    });
+  
+    newGroupNameInput.addEventListener("keypress", createNewButton)
+
+    function createNewButton(e){
+        if(e.key == "Enter"){
+            var buttonText = newGroupNameInput.value;
+
+            if (buttonText.trim !== '') {
+                var button = document.createElement('button');
+                button.textContent = buttonText;
+                existingGroupsContainer.appendChild(button);
+                toggleElement(newGroupNameInput,false);
+
+            }
+
+        }
+    }
+
+    addButton.addEventListener("click", function(){
+        toggleElement(existingGroupsContainer, true);
+    })
+  
+    // ... (implement logic to handle creating new groups and adding tabs to existing groups)
+  });
+  

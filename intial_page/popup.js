b26a8a6220b2708468a214ef08748d37ab985634
Bill Wang <18billwang@gmail.com>
2023-08-15 20:14:36 -0700

    guarantee async storage handling using promises

--- intial_page/popup.js
+++ intial_page/popup.js
@@ -1,32 +1,54 @@
-// Get current date and display it in the dailyStats element
-document.addEventListener('DOMContentLoaded', function () {
-    const dailyStatsElement = document.getElementById('dailyStats');
-    const todayHeading = document.getElementById('heading');
-    const currentDate = new Date().toLocaleDateString();
-    todayHeading.innerHTML = `<p>Today's Date: ${currentDate}</p>`;
+// popup.js
 
-    // Retrieve and display daily statistics
-    chrome.storage.local.get('spentToday', (result) => {
-        const timeData = result.spentToday;
-        const today = new Date().toLocaleDateString();
-
-        const dailyStatsElement = document.getElementById('dailyStats');
-        dailyStatsElement.innerHTML = `<h2>Daily Statistics</h2>`;
-        // dailyStatsElements.innerHTML = `<p>${today}</p>`;
-
-
-        if (timeData[today]) {
-            for (let domain in timeData[today]) {
-                const timeSpent = timeData[today][domain].totalTime;
+// Function to retrieve spentToday data from storage
+function getSpentTodayData() {
+    return new Promise((resolve, reject) => {
+      chrome.storage.local.get('spentToday', result => {
+        if (chrome.runtime.lastError) {
+          return reject(chrome.runtime.lastError);
+        }
+        resolve(result.spentToday || {});
+      });
+    });
+  }
+  
+  // Function to update the popup UI with the spentToday data
+  async function updatePopupUI() {
+    try {
+      const spentTodayData = await getSpentTodayData();
+  
+      // Get current date and display it in the dailyStats element
+      const dailyStatsElement = document.getElementById('dailyStats');
+      const todayHeading = document.getElementById('heading');
+      const currentDate = new Date().toLocaleDateString();
+      todayHeading.innerHTML = `<p>Today's Date: ${currentDate}</p>`;
+  
+      // Retrieve and display daily statistics
+      const today = new Date().toLocaleDateString();
+  
+      if (spentTodayData[today]) {
+        for (const domain in spentTodayData[today]) {
+            const timeSpent = spentTodayData[today][domain].totalTime;
+            if(domain.startsWith("www")){
                 dailyStatsElement.innerHTML += `<p>${domain}: ${timeSpent} ms</p>`;
             }
-        } else {
-            dailyStatsElement.innerHTML += `<p>No data available for today.</p>`;
+
         }
-    });
-
-//  
-
+      } else {
+        dailyStatsElement.innerHTML += `<p>No data available for today.</p>`;
+      }
+  
+      // Example: Display a simple message in the monthlyComparison element
+      const monthlyComparisonElement = document.getElementById('monthlyComparison');
+      monthlyComparisonElement.innerHTML = '<p>Monthly comparison data will be displayed here.</p>';
+    } catch (error) {
+      console.error('Error retrieving spentToday data:', error);
+    }
+  }
+  
+  // Call the updatePopupUI function to display the data when the popup is opened
+  document.addEventListener('DOMContentLoaded', updatePopupUI);
+  
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     const domainChangedDiv = document.getElementById('domain-changed');
     if (message.action === 'domainChanged') {
@@ -53,4 +75,4 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     // Example: Display a simple message in the monthlyComparison element
     const monthlyComparisonElement = document.getElementById('monthlyComparison');
     monthlyComparisonElement.innerHTML = '<p>Monthly comparison data will be displayed here.</p>';
-});
+
